b457959c647cf800340a62d0f8ca580878cd245c
Ricardo Fernandez <rafernandezjr@gmail.com>
2014-04-30 01:09:53 -0400

    Added linear regression example

---
+++ Examples/linear_regression.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cost_function(X, y, theta):
+
+    # number of training examples
+    m = y.shape[0]
+
+    error = np.dot(X, theta) - y
+
+    J = 0.5*np.dot(error, error)/m
+
+    return J
+
+def gradient_descent(X, y, theta, alpha, num_iterations):
+
+    # number of training examples
+    m = y.shape[0]
+    
+    X_T = X.T
+
+    for i in xrange(num_iterations):
+        error = np.dot(X, theta) - y
+        theta -= alpha*np.dot(X_T, error)/m
+
+# generate data - y = f(x) + e: f(x) = 2x + 1
+np.random.seed(1)
+x = np.random.normal(1., .5, 100)
+y = 2.*x + 1. + np.random.normal(0., .5, 100)
+
+alpha = 0.01          # strength of descent
+theta = np.zeros(2)   # initial theta
+
+# find theta that minimizes the cost function
+X = np.array([np.ones(x.size), x]).T
+gradient_descent(X, y, theta, alpha, 1500)
+
+
+plt.plot(x, y, 'or')
+plt.plot(x, 2.*x + 1, 'b')
+plt.plot(x, np.dot(X, theta), 'r')
+plt.xlim(0, 2)
+plt.ylim(0, 7)
+plt.show()
